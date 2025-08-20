07c6b7e63ca4fe1a7027e9609364cc6286081b7c
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-20 12:03:48 -0300

    add sql

--- src/migrations/m230420_193757_create_message_table.php
+++ src/migrations/m230420_193757_create_message_table.php
@@ -12,12 +12,15 @@ class m230420_193757_create_message_table extends Migration
      */
     public function safeUp()
     {
+        $sql = file_get_contents(__DIR__ . '/query_messages_insert.sql');
         $this->createTable('{{%message}}', [
             'id' => $this->integer()->notNull(),
             'language' => $this->string()->notNull(),
             'translation' => $this->text(),
         ]);
         $this->addPrimaryKey('id-pk','message',['id','language']);
+        $this->execute($sql);
+
     }
 
     
