7eb0bfc30d4645635023985e4733cc83558e5572
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-20 22:26:26 -0300

    temp disable auth

--- src/models/Configuration.php
+++ src/models/Configuration.php
@@ -130,9 +130,9 @@ class Configuration extends ModelCommon
         $model = self::find(false)->where(['host' => $hostName])->one();
 
         // Se não encontrar, busca pelo grupo do usuário
-        if (!$model) {
-            $model = self::find(false)->where(['id' => AuthController::userGroup()])->one();
-        }
+        // if (!$model) {
+        //     $model = self::find(false)->where(['id' => AuthController::userGroup()])->one();
+        // }
 
         // Se ainda não encontrar, tenta buscar o id 1
         if (!$model) {
