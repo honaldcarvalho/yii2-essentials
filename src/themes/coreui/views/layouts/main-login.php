c7360b0801562a3fbb9d242d46de86695c8381fa
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-21 03:46:12 -0300

    update

---
+++ src/themes/coreui/views/layouts/main-login.php
@@ -0,0 +1,45 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var string $content */
+
+use croacworks\essentials\controllers\CommonController;
+use croacworks\essentials\models\Configuration;
+use croacworks\essentials\themes\coreui\assets\CoreuiAsset;
+use croacworks\essentials\themes\coreui\assets\FontAwesomeAsset;
+use croacworks\essentials\themes\coreui\assets\PluginAsset;
+use yii\helpers\Html;
+
+CoreuiAsset::register($this);
+FontAwesomeAsset::register($this);
+PluginAsset::register($this)->add(['jquery','fontawesome','fancybox','sweetalert2']);
+$configuration = Configuration::get();
+$assetDir = Yii::$app->assetManager->getPublishedUrl('@vendor/croacworks/yii2-essentials/src/themes/coreui/web');
+// if(Yii::$app->user->identity === null){
+//     return (new CommonController(0,0))->redirect(['site/login']); 
+// }
+$theme = 'dark';//Yii::$app->user->identity->theme;
+
+?>
+<?php $this->beginPage() ?>
+<!doctype html >
+<html lang="<?= Yii::$app->language ?>" data-coreui-theme="dark">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title><?= $this->title != '' ? $configuration->title . ' - ' . Html::encode($this->title) : $configuration->title  ?></title>
+    <?php 
+    $this->head(); 
+    ?>
+  </head>
+  <body>
+    <?php $this->beginBody() ?>
+
+    <?= $this->render('content', ['content' => $content, 'assetDir' => $assetDir,'theme'=>$theme,'configuration'=>$configuration]) ?>
+
+    <?php $this->endBody() ?>
+  </body>
+</html>
+<?php $this->endPage() ?>
--- src/views/site/login.php
+++ src/views/site/login.php
@@ -10,23 +10,45 @@ use yii\bootstrap5\Html;
 $this->title = 'Login';
 ?>
 
-    <div class="col-lg-12">
+<div class="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="card-group d-block d-md-flex row">
+                    <div class="card col-md-7 p-4 mb-0">
+                        <div class="card-body">
+                            <h1>Login</h1>
+                            <p class="text-body-secondary">Sign In to your account</p>
+                            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                            <div class="input-group mb-3"><span class="input-group-text">
+                                    <svg class="icon">
+                                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
+                                    </svg></span>
+                                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                            </div>
+                            <div class="input-group mb-4"><span class="input-group-text">
+                                    <svg class="icon">
+                                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
+                                    </svg></span>
+                                 <?= $form->field($model, 'password')->passwordInput() ?>
+                            </div>
+                            <div class="input-group mb-4"><span class="input-group-text">
+                                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <button class="btn btn-primary px-4" type="button">Login</button>
+                                </div>
+                                <div class="col-6 text-end">
+                                    <?= Yii::t('app', ' If you forgot your password you can ') . Html::a('reset it', ['site/request-password-reset']) ?>.
+                                </div>
+                            </div>
+                            <?php ActiveForm::end(); ?>
+                        </div>
+                    </div>
 
-        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-            <?= $form->field($model, 'password')->passwordInput() ?>
-
-            <?= $form->field($model, 'rememberMe')->checkbox() ?>
-            <div class="my-1 mx-0 mb-3" style="color:#999;">
-                    <?= Yii::t('app',' If you forgot your password you can '). Html::a('reset it', ['site/request-password-reset']) ?>.
+                </div>
             </div>
-
-            <div class="form-group">
-                <?= Html::submitButton(Yii::t('app','Login'), ['class' => 'btn btn-weebz btn-block', 'name' => 'login-button']) ?>
-            </div>
-
-        <?php ActiveForm::end(); ?>
+        </div>
     </div>
-
+</div>
