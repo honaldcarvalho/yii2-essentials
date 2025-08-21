36f16eab5b77def57c4fb4e1aae2d744e5a3efab
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-21 01:39:40 -0300

    add parts

--- src/themes/coreui/views/layouts/content.php
+++ src/themes/coreui/views/layouts/content.php
@@ -1,5 +1,11 @@
 <?php
+
 use yii\bootstrap5\Alert;
 ?>
-<?= Alert::widget(); ?>
-<?=$content;?>
+
+<div class="wrapper d-flex flex-column min-vh-100">
+    <div class="body flex-grow-1">
+        <?= Alert::widget(); ?>
+        <?= $content; ?>
+    </div>
+</div>
