ca595a112f62a875fca995f3f16740bc3a3f7214
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-21 02:12:09 -0300

    add header

--- src/themes/coreui/views/layouts/content.php
+++ src/themes/coreui/views/layouts/content.php
@@ -4,6 +4,7 @@ use yii\bootstrap5\Alert;
 ?>
 
 <div class="wrapper d-flex flex-column min-vh-100">
+    <?= $this->render('header', ['assetDir' => $assetDir,'theme'=>$theme,'configuration'=>$configuration]) ?>
     <div class="body flex-grow-1">
         <?= Alert::widget(); ?>
         <?= $content; ?>
