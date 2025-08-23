2586b94551bed24a26cd7db2a5735c15c5f11b24
Honald Carvalho <honaldcarvalo@gmail.com>
2025-08-23 15:05:31 -0300

    fix

--- src/models/Configuration.php
+++ src/models/Configuration.php
@@ -45,16 +45,20 @@ class Configuration extends ModelCommon
 
     public function behaviors()
     {
-        return array_merge(parent::behaviors(), [
+        return [
             [
-                'class' => AttachFileBehavior::class,
-                'attribute' => 'file_id',
-                'removeFlagParam' => 'remove',
-                'deleteOldOnReplace' => true,
-                'deleteOnOwnerDelete' => false,
-                'debug' => true, // ligue por enquanto
+                'class' => \croacworks\essentials\behaviors\AttachFileBehavior::class,
+                'attribute'           => 'file_id',
+                'deleteOnOwnerDelete' => true,
+                'deleteOldOnReplace'  => true,
+                'thumbAspect'         => 1,
+                'folderId'            => 1,
+                'groupId'             => 1,
+                'saveModel'           => true,
+                'removeFlagParam'     => 'remove',
+                'removeFlagScoped'    => true, // se o hidden vier como Model[remove]
             ],
-        ]);
+        ];
     }
     
     public function scenarios()
