5e6af1c237d26151ce59c81b2ef16abed81e7ca3
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-01 19:48:13 -0300

    update

--- src/themes/coreui/views/layouts/main-login.php
+++ src/themes/coreui/views/layouts/main-login.php
@@ -12,34 +12,45 @@ use yii\helpers\Html;
 
 CoreuiAsset::register($this);
 FontAwesomeAsset::register($this);
-PluginAsset::register($this)->add(['fontawesome','fancybox','sweetalert2']);
+PluginAsset::register($this)->add(['fontawesome', 'fancybox', 'sweetalert2']);
 $configuration = Configuration::get();
 $assetDir = Yii::$app->assetManager->getPublishedUrl('@vendor/croacworks/yii2-essentials/src/themes/coreui/web');
 // if(Yii::$app->user->identity === null){
 //     return (new CommonController(0,0))->redirect(['site/login']); 
 // }
-$theme = 'dark';//Yii::$app->user->identity->theme;
+$theme = 'dark'; //Yii::$app->user->identity->theme;
 
 ?>
 <?php $this->beginPage() ?>
-<!doctype html >
+<!doctype html>
 <html lang="<?= Yii::$app->language ?>" data-coreui-theme="dark">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title><?= $this->title != '' ? $configuration->title . ' - ' . Html::encode($this->title) : $configuration->title  ?></title>
-    <?php 
-    $this->head(); 
-    ?>
-  </head>
-  <body>
-    <?php $this->beginBody() ?>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <?= $content; ?>
+  <title><?= $this->title != '' ? $configuration->title . ' - ' . Html::encode($this->title) : $configuration->title  ?></title>
+  <?php
+  $this->head();
+  ?>
+</head>
 
-    <?php $this->endBody() ?>
-  </body>
+<body>
+  <?php $this->beginBody() ?>
+  <div class="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <?= Alert::widget(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?= $content; ?>
+
+  <?php $this->endBody() ?>
+</body>
+
 </html>
 <?php $this->endPage() ?>
