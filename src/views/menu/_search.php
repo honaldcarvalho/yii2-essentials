27b41093ff98286ac4e84571df3044c27df33fec
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-07 16:25:53 -0300

    update

--- src/views/menu/_search.php
+++ src/views/menu/_search.php
@@ -4,40 +4,56 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model croacworks\essentials\models\MenuSearch */
+/* @var $model croacworks\essentials\models\Menu */
 /* @var $form yii\widgets\ActiveForm */
+
+$assetsDir = CommonController::getAssetsDir();
+$controllers = RoleController::getAllControllers();
+$text_select_controller = Yii::t('app', 'Select a controller');
+
+$script = <<< JS
+$('#controller-select').select2({ width: '100%', placeholder: $text_select_controller });
+JS;
+
+$this->registerJs($script);
+
+
 ?>
 
 <div class="row mt-2">
     <div class="col-md-12">
 
-    <?php $form = ActiveForm::begin([
-        'action' => ['index'],
-        'method' => 'get',
-    ]); ?>
+        <?php $form = ActiveForm::begin([
+            'action' => ['index'],
+            'method' => 'get',
+        ]); ?>
 
-    <?= $form->field($model, 'id') ?>
+        <?= $form->field($model, 'parent_id')->widget(\kartik\select2\Select2::classname(), [
+            'data' => yii\helpers\ArrayHelper::map(SysMenu::find()
+                ->asArray()->all(), 'id', 'label'),
+            'options' => ['multiple' => false, 'placeholder' => Yii::t('app', 'Select Menu')],
+            'pluginOptions' => [
+                'allowClear' => true,
+                'width' => '100%',
+            ],
+        ])->label('Menu');
+        ?>
 
-    <?= $form->field($model, 'parent_id') ?>
+        <?= $form->field($model, 'controller')->dropDownList($controllers, [
+            'id' => 'controller-select',
+            'prompt' => '-- Selecione o controller --'
+        ])->label('Controller') ?>
 
-    <?= $form->field($model, 'label') ?>
+        <?= $form->field($model, 'label')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'icon') ?>
+        <?= $form->field($model, 'status')->checkbox() ?>
 
-    <?= $form->field($model, 'visible') ?>
+        <div class="form-group">
+            <?= Html::submitButton('<i class="fas fa-search  mr-2"></i>' . Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+            <?= Html::resetButton('<i class="fas fa-broom mr-2 btn-reset"></i>' . Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+        </div>
 
-    <?php // echo $form->field($model, 'url') ?>
-
-    <?php // echo $form->field($model, 'active') ?>
-
-    <?php // echo $form->field($model, 'status') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('<i class="fas fa-search  mr-2"></i>' . Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('<i class="fas fa-broom mr-2"></i>' .Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
+        <?php ActiveForm::end(); ?>
 
     </div>
     <!--.col-md-12-->
--- src/views/menu/index.php
+++ src/views/menu/index.php
@@ -1,9 +1,6 @@
 <?php
 
 use yii\grid\GridView;
-use croacworks\essentials\widgets\DefaultButtons;
-use croacworks\essentials\components\gridview\ActionColumnCustom;
-use croacworks\essentials\controllers\AuthController;
 use croacworks\essentials\controllers\RoleController;
 use yii\bootstrap5\Html;
 use yii\widgets\Pjax;
@@ -82,72 +79,58 @@ $controllers = RoleController::getAllControllers(); // FQCNs
 $this::registerJs($script, $this::POS_END);
 
 ?>
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <div class="row mb-2">
-                        <div class="col-md-12">
-                            <div class="btn-group">
-                                <?= croacworks\essentials\widgets\DefaultButtons::widget([
-                                    'controller' => Yii::$app->controller->id,
-                                    'show' => ['create'],
-                                    'verGroup' => false
-                                ]) ?>
 
-                                <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#modal-auto-add">
-                                    <i class="fas fa-plus-circle"></i> Adicionar Automático
-                                </button>
-                            </div>
-                        </div>
-                    </div>
+<div class="menu-index">
 
-                    <?php // echo $this->render('_search', ['model' => $searchModel]); 
-                    ?>
-                    <?php Pjax::begin(['id' => 'grid-menu']); ?>
-                    <?= GridView::widget([
-                        'dataProvider' => $dataProvider,
-                        'columns' => [
-                            [
-                                'attribute' => 'label',
-                                'label' => 'Menu',
-                                'format' => 'raw',
-                                'value' => function ($model) {
-                                    /** @var \croacworks\essentials\models\SysMenu $model */
-                                    $count = $model->getChildren()->count();
-                                    $badge = $count ? " <span class='badge bg-secondary'>{$count}</span>" : '';
-                                    return \yii\helpers\Html::a(
-                                        \yii\helpers\Html::encode($model->label) . $badge,
-                                        ['view', 'id' => $model->id]
-                                    );
-                                }
-                            ],
-                            'icon',
-                            'order',
-                            'url:url',
-                            'status:boolean',
-                            [
-                                'class' => croacworks\essentials\components\gridview\ActionColumnCustom::class,
-                                'template' => '{status} {view} {update} {delete}',
-                                'uniqueId' => 'menu'
-                            ],
-                        ],
-                        'summaryOptions' => ['class' => 'summary mb-2'],
-                        'pager' => ['class' => 'yii\bootstrap5\LinkPager'],
-                    ]); ?>
-                    <?php Pjax::end(); ?>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-                </div>
-                <!--.card-body-->
-            </div>
-            <!--.card-->
-        </div>
-        <!--.col-md-12-->
-    </div>
-    <!--.row-->
+    <p>
+        <?= croacworks\essentials\widgets\DefaultButtons::widget([
+            'controller' => Yii::$app->controller->id,
+            'show' => ['create'],
+            'verGroup' => false
+        ]) ?>
+
+        <button class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#modal-auto-add">
+            <i class="fas fa-plus-circle"></i> <?= Yii::t('app', 'Add Automatic Menu'); ?>
+        </button>
+    </p>
+
+    <?php echo $this->render('/_parts/filter', ['view' => '/role-template', 'searchModel' => $searchModel]); ?>
+    <?php Pjax::begin(['id' => 'grid-menu']); ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            [
+                'attribute' => 'label',
+                'label' => 'Menu',
+                'format' => 'raw',
+                'value' => function ($model) {
+                    /** @var \croacworks\essentials\models\SysMenu $model */
+                    $count = $model->getChildren()->count();
+                    $badge = $count ? " <span class='badge bg-secondary'>{$count}</span>" : '';
+                    return \yii\helpers\Html::a(
+                        \yii\helpers\Html::encode($model->label) . $badge,
+                        ['view', 'id' => $model->id]
+                    );
+                }
+            ],
+            'icon',
+            'order',
+            'url:url',
+            'status:boolean',
+            [
+                'class' => croacworks\essentials\components\gridview\ActionColumnCustom::class,
+                'template' => '{status} {view} {update} {delete}',
+                'uniqueId' => 'menu'
+            ],
+        ],
+        'summaryOptions' => ['class' => 'summary mb-2'],
+        'pager' => ['class' => 'yii\bootstrap5\LinkPager'],
+    ]); ?>
+    <?php Pjax::end(); ?>
+
 </div>
-
 
 <div class="modal fade" id="modal-auto-add" tabindex="-1" aria-labelledby="modalAutoAddLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
