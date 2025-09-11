b58418192c9e4581fa7fb88f29c5eda2dc42a957
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-11 18:25:18 -0300

    fix

--- src/migrations/m250911_170000_add_slug_to_page_sections.php
+++ src/migrations/m250911_170000_add_slug_to_page_sections.php
@@ -13,10 +13,6 @@ class m250911_170000_add_slug_to_page_sections extends Migration
             $this->addColumn('{{%page_sections}}', 'slug', $this->string(64)->notNull()->after('id'));
         }
 
-        // índice único pro slug
-        // (tente um nome curto de índice)
-        $this->createIndex('ux_page_sections_slug', '{{%page_sections}}', ['slug'], true);
-
         // Popular slug para linhas existentes
         $rows = (new \yii\db\Query())
             ->from('{{%page_sections}}')
@@ -49,8 +45,6 @@ class m250911_170000_add_slug_to_page_sections extends Migration
 
     public function safeDown()
     {
-        // Se quiser manter a coluna, comente as duas linhas abaixo.
-        $this->dropIndex('ux_page_sections_slug', '{{%page_sections}}');
         $this->dropColumn('{{%page_sections}}', 'slug');
     }
 }
