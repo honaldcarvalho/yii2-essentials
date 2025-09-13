09347ec70344d48e21e827ae546c0682da65238f
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-13 17:35:49 -0300

    fix

--- src/themes/coreui/views/layouts/content.php
+++ src/themes/coreui/views/layouts/content.php
@@ -4,7 +4,7 @@ use croacworks\essentials\widgets\Alert;
 ?>
 
 <div class="wrapper d-flex flex-column min-vh-100">
-    <?= $this->render('header', ['assetDir' => $assetDir, 'theme' => $theme, 'configuration' => $configuration]) ?>
+    <?= $this->render('header', ['assetDir' => $assetDir, 'theme' => $theme, 'config' => $config]) ?>
     <div class="body flex-grow-1">
         <?= Alert::widget(); ?>
         <div class="container-xxl px-4">
