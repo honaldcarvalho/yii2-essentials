ed100bd85ce9a1656be909d506f865d7c3e736d0
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-18 03:39:03 -0300

    fix page

--- src/views/page/view.php
+++ src/views/page/view.php
@@ -2,6 +2,7 @@
 
 use croacworks\essentials\widgets\ListFiles;
 use croacworks\essentials\widgets\StorageUploadMultiple;
+use yii\bootstrap5\Html;
 use yii\widgets\DetailView;
 
 /* @var $this yii\web\View */
@@ -13,67 +14,44 @@ $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 
-<div class="container-fluid">
-    <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <p>
-                        <?= \croacworks\essentials\widgets\DefaultButtons::widget(['model'=>$model]) ?>
-                    </p>
-                    <?= DetailView::widget([
-                        'model' => $model,
-                        'attributes' => [
-                            'id',
-                            'group.name:text:'.Yii::t('app', 'Group'),
-                            'section.name:text:'.Yii::t('app', 'Page Section'),
-                            'slug',
-                            'title',
-                            'description',
-                            'content:raw',
-                            'keywords:ntext',
-                            'created_at:datetime',
-                            'status:boolean',
-                        ],
-                    ]) ?>
-                </div>
-                <!--.col-md-12-->
-            </div>
-            <!--.row-->
-        </div>
-        <!--.card-body-->
-    </div>
-    <!--.card-->
+<div class="user-view">
 
+    <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title"><?= Yii::t('app', 'Add File'); ?></h3>
-        </div>
+    <p>
+        <?= croacworks\essentials\widgets\DefaultButtons::widget(['model' => $model]) ?>
+    </p>
 
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-12">
-                    <?= StorageUploadMultiple::widget([
-                        'attach_model'=>[
-                            'class_name' => \croacworks\essentials\models\PageFile::class,
-                            'id'=> $model->id,
-                            'fields'=> ['page_id','file_id']
-                        ],
-                        'grid_reload'=>1,
-                        'grid_reload_id'=>'#list-files-grid'
-                    ]); ?>
-                </div>
-            </div>
-            <!--.row-->
-        </div>
-        <!--.card-->
-    </div>
-    <!--.card-->
-    
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'group.name:text:' . Yii::t('app', 'Group'),
+            'section.name:text:' . Yii::t('app', 'Page Section'),
+            'slug',
+            'title',
+            'description',
+            'content:raw',
+            'keywords:ntext',
+            'created_at:datetime',
+            'status:boolean',
+        ],
+    ]) ?>
+
+    <?= StorageUploadMultiple::widget([
+        'attach_model' => [
+            'class_name' => \croacworks\essentials\models\PageFile::class,
+            'id' => $model->id,
+            'fields' => ['page_id', 'file_id']
+        ],
+        'grid_reload' => 1,
+        'grid_reload_id' => '#list-files-grid'
+    ]); ?>
+
     <?= ListFiles::widget([
         'dataProvider' => new \yii\data\ActiveDataProvider([
             'query' => $model->getFiles(),
         ]),
     ]); ?>
+    
 </div>
