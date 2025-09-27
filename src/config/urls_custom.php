676852af0b1fe039c237acf700c0fe7a5c1db6b5
Honald Carvalho <honaldcarvalo@gmail.com>
2025-09-26 23:33:20 -0300

    update

--- src/config/urls_custom.php
+++ src/config/urls_custom.php
@@ -1,12 +1,24 @@
 <?php
 
-function customControllersUrl($controllers,$folder = 'custom'){
+function customControllersUrl(array $controllers, string $folder = 'custom'): array
+{
     $rules = [];
-    foreach ($controllers as $key => $controller) {	
-        $rules["{$controller}/<id:\d+>"] = "{$folder}/{$controller}/view";
-        $rules["{$controller}/<action>/<id:\d+>"] = "{$folder}/{$controller}/<action>";
-        $rules["{$controller}/<action>"] = "{$folder}/{$controller}/<action>";
-        $rules["{$controller}"] = "{$folder}/{$controller}";
+
+    foreach ($controllers as $controller) {
+        $c = trim((string)$controller, "/ \t\n\r\0\x0B");
+        if ($c === '') {
+            continue; // evita blog//...
+        }
+        // aceita letras, números e hífen no controller id (ex.: post-section)
+        if (!preg_match('~^[a-z0-9\-]+$~', $c)) {
+            continue;
+        }
+
+        $rules["{$c}/<id:\d+>"]                 = "{$folder}/{$c}/view";
+        $rules["{$c}/<action:[\w\-]+>/<id:\d+>"] = "{$folder}/{$c}/<action>";
+        $rules["{$c}/<action:[\w\-]+>"]          = "{$folder}/{$c}/<action>";
+        $rules["{$c}"]                           = "{$folder}/{$c}";
     }
+
     return $rules;
 }
