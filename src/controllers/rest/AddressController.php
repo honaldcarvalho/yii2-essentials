dffbbb6c063124bb28cce7adf31f89ea149dd78f
Honald Carvalho <honaldcarvalo@gmail.com>
2025-10-06 12:32:58 -0300

    fix

--- src/controllers/rest/AddressController.php
+++ src/controllers/rest/AddressController.php
@@ -7,16 +7,17 @@ use Yii;
 
 use croacworks\essentials\models\City;
 use croacworks\essentials\models\State;
+use yii\rest\Controller;
 use yii\web\BadRequestHttpException;
 
-class AddressController extends AuthorizationController {
+class AddressController extends Controller {
     
     public function __construct($id, $module, $config = array())
     {
         parent::__construct($id, $module, $config);
         $this->free = ['cities', 'states'];
     }
-    
+
     public $serializer = [
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
