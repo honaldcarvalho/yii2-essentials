832ceae66ef6b3ef1142e7efb1489be27d48838c
Honald Carvalho <honaldcarvalo@gmail.com>
2025-11-03 12:12:27 -0300

    fix

--- src/migrations/m251101_210000_add_list_to_page_sections.php
+++ src/migrations/m251101_210000_add_list_to_page_sections.php
@@ -10,19 +10,14 @@ class m251101_210000_add_list_to_page_sections extends Migration
 {
     public function safeUp()
     {
-        if ($this->db->getTableSchema('{{%pages}}', true) !== null) {
-            if (!$this->hasColumn('{{%page_sections}}', 'list')) {
-                $this->addColumn('{{%page_sections}}', 'list', $this->boolean()->defaultValue(true)->after('uri'));
-                $this->createIndex('idx-page_sections-list', '{{%page_sections}}', 'list');
-            }
-        }
+
+        $this->addColumn('{{%page_sections}}', 'list', $this->boolean()->defaultValue(true)->after('uri'));
+        $this->createIndex('idx-page_sections-list', '{{%page_sections}}', 'list');
     }
 
     public function safeDown()
     {
-        if (!$this->hasColumn('{{%page_sections}}', 'list')) {
-            $this->dropIndexIfExists('idx-page_sections-list', '{{%page_sections}}', 'list');
-            $this->dropColumn('{{%page_sections}}', 'list', $this->boolean()->defaultValue(true)->after('uri'));
-        }
+        $this->dropIndexIfExists('idx-page_sections-list', '{{%page_sections}}', 'list');
+        $this->dropColumn('{{%page_sections}}', 'list');
     }
 }
