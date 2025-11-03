5c70baacd3a63594219018b33bf87c49474ac61d
Honald Carvalho <honaldcarvalo@gmail.com>
2025-11-03 14:39:29 -0300

    fix

--- src/migrations/m250911_170000_add_live_notifications_to_user_profiles.php
+++ src/migrations/m250911_170000_add_live_notifications_to_user_profiles.php
@@ -6,7 +6,7 @@ class m250911_170000_add_live_notifications_to_user_profiles extends Migration
 {
     public function safeUp()
     {
-        $this->addColumn('{{%user_profiles}}', 'live_notifications', $this->boolean()->defautValue(false)->notNull()->after('id'));   
+        $this->addColumn('{{%user_profiles}}', 'live_notifications', $this->boolean()->defaultValue(false)->notNull()->after('id'));   
         $this->addColumn('{{%user_profiles}}', 'notifications_interval', $this->integer()->defaultValue(60000)->notNull()->after('id'));   
     }
     public function safeDown()
