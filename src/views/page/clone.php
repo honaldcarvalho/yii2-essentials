d2c732dcf19437b955b770107f07f18d6964becf
Honald Carvalho <honaldcarvalo@gmail.com>
2025-11-04 08:23:08 -0300

    update

---
+++ src/views/page/clone.php
@@ -0,0 +1,46 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $model croacworks\essentials\models\Page */
+
+use yii\bootstrap5\Html;
+
+$this->title = Yii::t('app', 'Clone Page: {name}', [
+    'name' => $model->title,
+]);
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Pages'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = Yii::t('app', 'Clone');
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <div class="card">
+                <div class="card-body">
+
+                    <div class="row mb-2">
+                        <div class="col-md-12">
+                            <?= croacworks\essentials\widgets\DefaultButtons::widget([
+                                'show' => ['index'],
+                                'buttons_name' => ['index' => Yii::t('app', 'List') . ' ' . Yii::t('app', 'Pages')]
+                            ]) ?>
+                        </div>
+                    </div>
+
+                    <?= $this->render('_form', [
+                        'model' => $model
+                    ]) ?>
+
+                </div>
+                <!--.card-body-->
+            </div>
+            <!--.card-->
+        </div>
+        <!--.col-md-12-->
+    </div>
+    <!--.row-->
+</div>
